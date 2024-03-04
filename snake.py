52d1b3765eb26ecbf87f5fb256d353c501f1f16a
Manuel Leguizamon <leguimanu@hotmail.com>
2024-03-04 00:17:21 -0300

    2°-> Movimiento; Comida

--- snake.py
+++ snake.py
@@ -1,47 +1,70 @@
 import pygame as pg
-WIDTH_WIN, HEIGHT_WIN = 400, 600
-WIN = pg.display.set_mode((HEIGHT_WIN, WIDTH_WIN))
-pg.display.set_caption("La Bovirita!")
-#---------------------------------------------------V
-#   Variables:
-snake_size=20
-snake_color = (153, 153, 153)
-move = 7
+from random import randint
 
+HEIGHT_WIN,WIDTH_WIN = 800, 600
+WIN = pg.display.set_mode((HEIGHT_WIN, WIDTH_WIN))
+pg.display.set_caption("La Bovira!")
+#---------------------------------------------------V
+#   Constantes:
+snake_size=25
+snake_color = (153, 153, 153)
+move = 5
+
+food_color = (51, 77, 0)
 WIN_COLOR = (13, 13, 13)
 #---------------------------------------------------V
 #   SNAKE MOVMENT
-def movimiento(key_pressed, SNAKE):
-    if key_pressed[pg.K_w]:
-        SNAKE.y -= move  
-    elif key_pressed[pg.K_s]:
-        SNAKE.y += move  
-    elif key_pressed[pg.K_a]:
-        SNAKE.x -= move  
-    elif key_pressed[pg.K_d]:
-        SNAKE.x += move  
+def movimiento(SNAKE, DIRECCION):
+        if DIRECCION == "up" and SNAKE.y - move > 0:
+            SNAKE.y -= move  
+        elif DIRECCION == "down" and SNAKE.y + move < WIDTH_WIN - snake_size:
+            SNAKE.y += move  
+        elif DIRECCION == "left" and SNAKE.x - move > 0:
+            SNAKE.x -= move  
+        elif DIRECCION == "right" and SNAKE.x + move < HEIGHT_WIN - snake_size:
+            SNAKE.x += move  
+    
 
 #---------------------------------------------------V
-def draw_win(SNAKE):
+def draw_win(SNAKE,FOOD):
     WIN.fill(WIN_COLOR)
     pg.draw.rect(WIN, snake_color, SNAKE)
+    pg.draw.rect(WIN,food_color,FOOD)
     pg.display.update()
 
 #---------------------------------------------------V
-
 def main():
 
-    SNAKE = pg.Rect(300, 200, snake_size, snake_size)
-    key_pressed = pg.key.get_pressed()
+    SNAKE = [pg.rect.Rect(300, 200, snake_size, snake_size)]
+    DIRECCION = ''
+    FOOD = pg.Rect(randint(0, WIDTH_WIN - snake_size), randint(0, HEIGHT_WIN - snake_size),snake_size,snake_size)
     clock = pg.time.Clock()
     while True:
+        key_pressed = pg.key.get_pressed()
         clock.tick(60)
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
-            movimiento(key_pressed, SNAKE)
-        draw_win(SNAKE)
+# Movimiento
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_w:
+                    DIRECCION = "up"
+                elif event.key == pg.K_s:
+                    DIRECCION = "down"
+                elif event.key == pg.K_a:
+                    DIRECCION = "left"
+                elif event.key == pg.K_d:
+                    DIRECCION = "right"
+
+        NEW_SEGMENT = pg.Rect(SNAKE[0].x, SNAKE[0].y, snake_size, snake_size)
+        if SNAKE.colliderect(FOOD):
+            SNAKE.append(NEW_SEGMENT)
+            FOOD.x = randint(0, WIDTH_WIN - snake_size)
+            FOOD.y = randint(0, HEIGHT_WIN - snake_size)
+
+        movimiento(SNAKE, DIRECCION)
+        draw_win(SNAKE,FOOD)  
 
 main()
 
